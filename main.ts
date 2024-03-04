dd9ba440261020e5c658033f49e8322004c1b8b2
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-04 23:43:02 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,1 @@
+let paragragh = ("Karachi is known as the city of light.Karachi is the largest city of Pakistan.It is the capital province of Sindh.It is located on the shores of the Arabian Sea.Karachi was as small fishing village known as Karachi-jo-Goth.");

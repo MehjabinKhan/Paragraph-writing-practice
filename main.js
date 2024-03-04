e53dfcffeefad1f555fd48a3b59f12b63f2af67d
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-04 23:55:40 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-var paragragh = ("Karachi is known as the city of light.Karachi is the largest city of Pakistan.It is the capital province of Sindh.It is located on the shores of the Arabian Sea.Karachi was as small fishing village known as Karachi-jo-Goth.");
+var Karachi1 = ("Karachi is known as the city of light.Karachi is the largest city of Pakistan.It is the capital province of Sindh.It is located on the shores of the Arabian Sea.Karachi was as small fishing village known as Karachi-jo-Goth.");
+console.log(Karachi1);
--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
-let paragragh = ("Karachi is known as the city of light.Karachi is the largest city of Pakistan.It is the capital province of Sindh.It is located on the shores of the Arabian Sea.Karachi was as small fishing village known as Karachi-jo-Goth.");
+let Karachi = ("Karachi is known as the city of light.Karachi is the largest city of Pakistan.It is the capital province of Sindh.It is located on the shores of the Arabian Sea.Karachi was as small fishing village known as Karachi-jo-Goth.");
+console.log(Karachi);
